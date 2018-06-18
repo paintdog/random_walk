0dac1140e73f931e48487d779008b1689c1fc867
paintdog <paintdog@users.noreply.github.com>
2018-06-18 22:54:13 +0200

    Update random_walk.py

--- random_walk.py
+++ random_walk.py
@@ -2,27 +2,21 @@ import matplotlib.pyplot as plt
 from random import choice, randint
 
 
-upper_limit = 25
+upper_limit = 12
+value = 10           # initial value
+
 
 change = [(i / 10) for i in range(1, upper_limit)]
-
-value = 10
 values = []
 
 
-for i in range(960):
-
+for i in range(220):
     if randint(0,1):
-
         value += choice(change)
-
     else:
-
         value -= choice(change)
-
         if value < 0:
             value *= -1
-
     values.append(value)
 
         
